ca26789dcb6b53d5d407dc27e27a9774aaf84618
nhsjgczryf <wnhkcs@gmail.com>
2020-09-15 22:09:20 +0800

    delete redundant files

--- scripts/.ipynb_checkpoints/ace2005_50-checkpoint.sh
+++
@@ -1,7 +0,0 @@
-python train.py \
---eval \
---train_path ./data/cleaned_data/ACE2005/50_mini_train.json \
---dev_path ./data/cleaned_data/ACE2005/50_mini_train.json  \
---pretrained_model_path bert-base-cased \
---warmup_ratio -1  \
---lr 8e-6
